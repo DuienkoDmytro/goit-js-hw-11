8e2ef7466b475f31970a6a367b90fa852854bc9e
DuienkoDmytro <119892965+DuienkoDmytro@users.noreply.github.com>
2023-06-06 00:00:18 +0300

    fixed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import { getImages } from "./services/api";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import SimpleLightbox from "simplelightbox";
 
 const form = document.querySelector("#search-form");
 const container = document.querySelector(".gallery")
@@ -11,21 +10,34 @@ btn.addEventListener("click", onClick);
 
 let page = 1;
 let value = "";
- 
 
 function onSubmit(e) {
     e.preventDefault();
     page = 1;
     container.innerHTML = "";
     const { searchQuery } = e.currentTarget.elements;
-    const value = searchQuery.value;
-  getImages(value, page).then((data) => {
-    if (data.hits.length === 0) { Notify.failure("Sorry, there are no images matching your search query. Please try again."); btn.classList.add("is-hidden"); }
-    else
-    { createMarkup(data); { Notify.success(`Hooray! We found ${data.totalHits} images.`) } }
-    
-  })
+  value = searchQuery.value.trim();
+    if (!value) {
+      Notify.failure("Sorry, you can`t search with clear field. Please type something.");
+            return;
+    }
+  getImages(value, page).then(({ data }) => {
+    if (data.hits.length < 40) {
+      btn.classList.add("is-hidden")
+    }
+    else {
+      btn.classList.remove("is-hidden")
+    }
+    if (data.hits.length === 0) {
+      Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+       }    
+  else
+    {
+      createMarkup(data); { Notify.success(`Hooray! We found ${data.totalHits} images.`) }
+    }    
+  })  
 }
+
 function createMarkup({hits}) {
     const markup=hits?.map (({webformatURL, tags, likes, views, comments, downloads  })=> `<div class="photo-card">
   <img src="${webformatURL}" alt="${tags}" loading="lazy" width="330" height="280"/>
@@ -44,13 +56,18 @@ function createMarkup({hits}) {
     </p>
   </div>
 </div>`).join('')
-  container.insertAdjacentHTML("beforeend", markup);
- { btn.classList.remove("is-hidden") }   
+  container.insertAdjacentHTML("beforeend", markup); 
 }
 
 function onClick(e) {
-    page += 1;
-  getImages(value, page).then((data) => { console.log(data); if (data !== undefined) { createMarkup(data) } } )
+  page += 1;  
+  getImages(value, page).then(({ data }) => {
+    console.log(data);
+    if (data !== undefined)
+    { createMarkup(data) };    
+    if (page < data.totalHits / 40)
+    { btn.classList.remove("is-hidden") }
+    else 
+    { btn.classList.add("is-hidden") };
+  })  
 }
-
-//  const lightbox = new SimpleLightbox('.photo-card', { captionDelay: "250", captionsData: "alt"});
--- src/services/api.js
+++ src/services/api.js
@@ -1,16 +1,26 @@
 import axios from "axios";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const API_KEY = "37014640-9c46812dfd82041fe339e82c5";
-axios.defaults.baseURL = "https://pixabay.com/api/";
 
-export const getImages = async (photo, currentPage) => { try {
-    const { data } = await axios(`?key=${API_KEY}&q=${photo}&page=${currentPage}&image_type=photo&per_page=100&orientation=horizontal&safesearch=true`)
-    return data;
-} catch (error) {
-    Notify.failure("We're sorry, but you've reached the end of search results.");
+const btn = document.querySelector(".load-more");
+const API_KEY = "37014640-9c46812dfd82041fe339e82c5";
+const BASE_URL = "https://pixabay.com/api/";
+
+export const getImages = async (photo, currentPage) => {
+    const params = new URLSearchParams({
+        key: API_KEY,
+        q: photo,
+        image_type: `photo`,
+        orientation: `horizontal`,
+        safesearch: true,
+        per_page: 40,
+        page: currentPage,
+    })
+    return await axios(`${BASE_URL}?${params}`);
 }
-}
+
+
+
 
 
 
